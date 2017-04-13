ce65b57b2d2f725e9cbd5090dd35249608fe6d5f
Monal Shadi <monalshadi16@gmail.com>
2017-04-14 04:58:42 +0530

    Added favicon. Added scrolling within webpage. Fixed indentations. Added clock and page-hit count.

---
+++ JS/Main.js
@@ -0,0 +1,114 @@
+/**
+ * Created by Monal on 4/14/2017.
+ */
+function nameDefined(ckie, nme) {
+    var splitValues;
+    var i;
+    for (i = 0; i < ckie.length; ++i) {
+        splitValues = ckie[i].split("=");
+        if (splitValues[0] == nme) return true
+    }
+    return false
+}
+
+function delBlanks(strng) {
+    var result = "";
+    var i;
+    var chrn;
+    for (i = 0; i < strng.length; ++i) {
+        chrn = strng.charAt(i);
+        if (chrn != " ") result += chrn
+    }
+    return result
+}
+function getCookieValue(ckie, nme) {
+    var splitValues;
+    var i;
+    for (i = 0; i < ckie.length; ++i) {
+        splitValues = ckie[i].split("=");
+        if (splitValues[0] == nme) return splitValues[1]
+    }
+    return ""
+}
+function insertCounter() {
+    readCookie();
+    displayCounter()
+}
+function displayCounter() {
+    document.write('<H3 ALIGN="CENTER">');
+    document.write("You've visited this page ");
+    if (counter == 1) document.write("the first time.");
+    else document.write(counter + " times.");
+    document.writeln('</H3>')
+}
+function readCookie() {
+    var cookie = document.cookie;
+    counter = 0;
+    var chkdCookie = delBlanks(cookie);  //are on the client computer
+    var nvpair = chkdCookie.split(";");
+    if (nameDefined(nvpair, "pageCount"))
+        counter = parseInt(getCookieValue(nvpair, "pageCount"));
+    ++counter;
+    var futdate = new Date();
+    var expdate = futdate.getTime();
+    expdate += 3600000 * 24 * 30;  //expires in 1 hour
+    futdate.setTime(expdate);
+
+    var newCookie = "pageCount=" + counter;
+    newCookie += "; expires=" + futdate.toGMTString();
+    window.document.cookie = newCookie
+}
+
+function displayTime() {
+    var date = new Date();
+    var h = date.getHours();
+    var m = date.getMinutes();
+    var s = date.getSeconds();
+    var session = "AM";
+    if (h == 0) {
+        h = 12;
+    }
+    if (h > 12) {
+        h = h - 12;
+        session = "PM";
+    }
+    if (h < 10) {
+        h = "0" + h;
+    }
+    if (m < 10) {
+        m = "0" + m;
+    }
+    if (s < 10) {
+        s = "0" + s;
+    }
+    var time = h + ":" + m + ":" + s + " " + session;
+    document.getElementById("time").innerText = time;
+    document.getElementById("time").textContent = time;
+    setTimeout(displayTime, 1000);
+}
+
+window.smoothScroll = function (target) {
+    var scrollContainer = target;
+    do { //find scroll container
+        scrollContainer = scrollContainer.parentNode;
+        if (!scrollContainer) return;
+        scrollContainer.scrollTop += 1;
+    } while (scrollContainer.scrollTop == 0);
+
+    var targetY = 0;
+    do { //find the top of target relatively to the container
+        if (target == scrollContainer) break;
+        targetY += target.offsetTop;
+    } while (target = target.offsetParent);
+
+    scroll = function (c, a, b, i) {
+        i++;
+        if (i > 30) return;
+        c.scrollTop = a + (b - a) / 30 * i;
+        setTimeout(function () {
+            scroll(c, a, b, i);
+        }, 20);
+    };
+    // start scrolling
+    scroll(scrollContainer, scrollContainer.scrollTop, targetY, 0);
+};
--- JS/count.js
+++ JS/count.js
@@ -1,7 +1,9 @@
 function init() {
     var n = 0;
     e = document.getElementById("output");
-    setInterval(function() { e.innerHTML = ++n; }, 1000);
+    setInterval(function () {
+        e.innerHTML = ++n;
+    }, 1000);
 }
 
 window.onload = init;
